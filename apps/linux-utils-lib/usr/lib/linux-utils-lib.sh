e1287efd56c4f9b4b29065e92fed37b32911169b
szarkii <szarkii@protonmail.com>
2023-01-28 20:01:43 +0100

    Add frames generator
    * Update library with logger functions

--- apps/linux-utils-lib/usr/lib/linux-utils-lib.sh
+++ apps/linux-utils-lib/usr/lib/linux-utils-lib.sh
@@ -18,3 +18,27 @@ function lib_printHelpAndExit {
     echo -e "$HELP"
     exit 1
 }
+
+lib_getFormattedTime() {
+    date '+%Y-%m-%d %H:%M:%S_%N' | cut -c 1-23
+}
+
+lib_logInfo() {
+    echo "[INFO] $(lib_getFormattedTime) $1"
+}
+
+lib_logWarn() {
+    echo "[WARN] $(lib_getFormattedTime) $1"
+}
+
+lib_logSuccess() {
+    echo "[SUCC] $(lib_getFormattedTime) $1"
+}
+
+lib_logError() {
+    echo "[ERRO] $(lib_getFormattedTime) $1"
+}
+
+lib_logSeparator() {
+    echo
+}
